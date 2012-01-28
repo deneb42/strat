ad3a3f2f95abe55a8a4b8f8021ed9d49d744afcc
deneb <drbenj@free.fr>
2012-01-28 03:21:40 +0100

    preparing the game's ui

---
+++ Strat-Andro/src/GameUI.java
@@ -0,0 +1,12 @@
+import org.globalgamejam.strat.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class GameUI extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.client_layout);
+	}
+}
